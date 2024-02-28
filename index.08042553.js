a75db2b7f916392562979ac51ff531c780464feb
Davwidos <d.jurkowski015@gmail.com>
2024-02-28 14:23:26 +0100

    make build

---
+++ index.08042553.js
@@ -0,0 +1,2 @@
+!function(r,e){Array.from(r.querySelectorAll("tr")).forEach(function(r){var e=Array.from(r.children),o=e[1].cloneNode(!0);e[e.length-1].before(o)})}(document.querySelector("table"),0);
+//# sourceMappingURL=index.08042553.js.map
